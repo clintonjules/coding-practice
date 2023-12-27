322d865c9ee8e676e8d0a92e4f0525a7e6c6c116
Clint <13054455+clintonjules@users.noreply.github.com>
2023-12-26 21:10:08 -0500

    Create city_name_ends_with_vowel.sql

---
+++ hackerrank/sql_practice/city_name_ends_with_vowel.sql
@@ -0,0 +1,6 @@
+/*
+Enter your query here.
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY RLIKE '.*[aeiouAEIOU]$'
