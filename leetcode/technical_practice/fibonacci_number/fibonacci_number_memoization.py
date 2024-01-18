960bae8a8360db0715f8b125ec9778286e2d966d
Clint <13054455+clintonjules@users.noreply.github.com>
2024-01-17 21:24:15 -0500

    Update fibonacci_number_memoization.py

--- leetcode/technical_practice/fibonacci_number/fibonacci_number_memoization.py
+++ leetcode/technical_practice/fibonacci_number/fibonacci_number_memoization.py
@@ -2,10 +2,12 @@ def fib(self, n: int) -> int:
     if n == 0:
         return 0
 
+    # Use n+1 to include 0 also
     cache = [None] * (n + 1)
     cache[0] = 0
     cache[1] = 1
-    
+
+    # Use n+1 since the range function excludes the upper limit
     for i in range(2,n+1):
         cache[i] = cache[i-1] + cache[i-2]
 
