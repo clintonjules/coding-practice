cd7dfb75f7a42be157965c79e2997627d4b144ca
Clint <13054455+clintonjules@users.noreply.github.com>
2024-01-18 21:55:58 -0500

    Create two_sum.py

---
+++ leetcode/technical_practice/two_sum/two_sum.py
@@ -0,0 +1,11 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    cache = []
+
+    for i, n in enumerate(nums):
+        cache.append(n)
+
+        check = target - n
+        if check in cache and cache.index(check) != i:
+            return [i, cache.index(check)]
+
+    return [] 
