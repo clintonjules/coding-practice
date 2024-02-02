baf6b22ed11e8b8bbe6b52ea288ae71fb3d2d550
Clint <13054455+clintonjules@users.noreply.github.com>
2024-02-01 19:39:27 -0500

    Update rank_scores.py

--- leetcode/pandas_practice/rank_scores.py
+++ leetcode/pandas_practice/rank_scores.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 # Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
-
 # The scores should be ranked from the highest to the lowest.
 # If there is a tie between two scores, both should have the same ranking.
 # After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
