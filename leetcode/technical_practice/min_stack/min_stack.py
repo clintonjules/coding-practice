fe4dddf5154b83cab54f881f72b74dd0997e82ab
Clint <13054455+clintonjules@users.noreply.github.com>
2024-10-08 14:42:55 -0500

    Create min_stack.py

---
+++ leetcode/technical_practice/min_stack/min_stack.py
@@ -0,0 +1,16 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.insert(0, val)
+
+    def pop(self) -> None:
+        return self.stack.pop(0)
+
+    def top(self) -> int:
+        return self.stack[0]
+
+    def getMin(self) -> int:
+        return min(self.stack)
